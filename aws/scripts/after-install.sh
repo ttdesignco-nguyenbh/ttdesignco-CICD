a88d06853fcf81c24a6be5bb3a4e532c56f53e1c
nguyenbh <buihoangnguyenhb@gmail.com>
2022-08-26 13:51:22 +0700

    fix

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # copy forder build from S3 bucket to /home/ec2-user/ttdesignco-cicd
-aws s3 cp s3://ttd-develop/ttd-code-upload-S3/build/* /usr/share/nginx/html
+mv /home/ec2-user/TTD/TTD/ttdesignco-cicd/ttdesignco-cicd/build/* /usr/share/nginx/html/
 
 # Ensure the ownership permissions are correct
 # chown -R root.root /home/ec2-user/ttdesignco-cicd
