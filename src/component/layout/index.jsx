237d1097650e9ab85d3fe525c5cae2c791c7c3f3
ttdesignco-nguyenbh <111562651+ttdesignco-nguyenbh@users.noreply.github.com>
2022-08-31 04:31:02 +0700

    Update index.jsx

--- src/component/layout/index.jsx
+++ src/component/layout/index.jsx
@@ -27,7 +27,7 @@ function handleToggleOpen () {
                 <div className="logo" />
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
                     <Menu.Item key="1" icon={<VideoCameraOutlined />}>
-                        Dashboard
+                        Console
                     </Menu.Item>
                     <Menu.Item key="2" icon={ <UserOutlined />}>
                         Người dùng
